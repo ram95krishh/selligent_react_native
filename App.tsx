7dfdb33303acdde78aaadff49ef14fa0d495c780
Rama <ramakrishnan.cs@recharge.com>
2024-10-04 17:21:46 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -6,57 +6,127 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+import Selligent from '@selligent-marketing-cloud/selligent-react-native';
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import { useEffect, useState } from 'react';
+import Toast from 'react-native-toast-message';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+let getDeviceIdPromise: Promise<string> | undefined;
+
+export async function getDeviceId() {
+  if (getDeviceIdPromise == null) {
+    throw new Error('Selligent Device ID is not populated yet.');
+  }
+  return await getDeviceIdPromise;
 }
+
+enum SelligentBroadcastEventType {
+  /** A button was clicked */
+  BUTTON_CLICKED = 'ButtonClicked',
+  /** An in-app message has been received */
+  RECEIVED_IN_APP_MESSAGE = 'ReceivedInAppMessage',
+  /** A notification will be displayed */
+  WILL_DISPLAY_NOTIFICATION = 'WillDisplayNotification',
+  /** A notification will be dismissed */
+  WILL_DISMISS_NOTIFICATION = 'WillDismissNotification',
+  /** A device id has been received */
+  RECEIVED_DEVICE_ID = 'ReceivedDeviceId',
+  /** Received GCM TOKEN */
+  RECEIVED_GCM_TOKEN = 'ReceivedGCMToken',
+  /** Received a remote notification */
+  RECEIVED_REMOTE_NOTIFICATION = 'ReceivedRemoteNotification',
+  /** Received an universal link execution */
+  UNIVERSAL_LINK_EXECUTED = 'UniversalLinkExecuted',
+  /** A custom event has been triggered */
+  TRIGGERED_CUSTOM_EVENT = 'TriggeredCustomEvent',
+  /** An IAM is about to be displayed (when `customInAppUi` is set to `true` in `selligent.json`) */
+  DISPLAYING_IN_APP_MESSAGE = 'DisplayingInAppMessage'
+}
+
+export async function getDeviceIdFromSelligent() {
+  return new Promise<string>((resolve, reject) => {
+    console.log('getDeviceId start', performance.now());
+    Selligent.getDeviceId((deviceId: string) => {
+      if (deviceId !== '') {
+        console.log('getDeviceId end', performance.now() + deviceId);
+        resolve(deviceId);
+      } else {
+        Toast.show({ text1: 'Did not receive deviceID from getDeviceID'});
+        reject(new Error('Selligent.getDeviceId() did not return deviceUID'));
+      }
+    });
+  });
+}
+
+type TSelligentBroadcastEvent = {
+  data: {
+    deviceId: string
+  },
+  broadcastEventType: string
+}
+
+export const initializeDeviceId = () => {
+  getDeviceIdPromise = Promise.any([
+    getDeviceIdFromSelligent(),
+    Promise.race([
+      new Promise<string>((resolve) => {
+        let deviceIdSubscriptionFulfilled = false;
+        console.log('subscribeToEvent', performance.now());
+        Selligent.subscribeToEvent((event: TSelligentBroadcastEvent) => {
+          Toast.show({ text1: 'DeviceID came from subscription' + event.data.deviceId});
+          console.log('received event from subscribeToEvent at', performance.now(), event.data.deviceId);
+          const { deviceId } = event.data;
+          if (deviceIdSubscriptionFulfilled) {return;}
+          if (deviceId !== '' && typeof deviceId !== 'object') {
+            resolve(deviceId);
+            deviceIdSubscriptionFulfilled = true;
+          }
+        }, SelligentBroadcastEventType.RECEIVED_DEVICE_ID);
+      }),
+      new Promise<never>((_resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error('Selligent Device ID resolution timed out.'));
+        }, 65000);
+      }),
+    ]),
+  ]).catch((err:any) => {
+    console.log('Something went wrong during initialization of device ID', err.message);
+    Toast.show({ text1: 'Something went wrong during initialization of device ID' + err.message});
+    throw err;
+  });
+};
+
+
+
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const [deviceId, setDeviceId] = useState<string | undefined>();
+
+  useEffect(() => {
+    initializeDeviceId();
+    getDeviceIdPromise?.then(selligentDeviceId => {
+      if (selligentDeviceId === '' || selligentDeviceId == null) {
+        Toast.show({ type: 'error', text1: "Didn't receive a device ID from Selligent" });
+      }
+      console.log('selligentDeviceId', selligentDeviceId);
+      setDeviceId(selligentDeviceId);
+    });
+  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -76,43 +146,13 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text>
+            Retrieved device ID:: {deviceId}
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- android/app/src/main/java/com/selligent/MainApplication.kt
+++ android/app/src/main/java/com/selligent/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.selligent.RNSelligent
 
 class MainApplication : Application(), ReactApplication {
 
@@ -39,5 +40,6 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+    RNSelligent.configure(this)
   }
 }
